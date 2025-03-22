3cbd1268396d7cb1b030d223f2d50705b89e27dd
lmunarim <lmunarim@gmail.com>
2025-03-22 20:07:10 -0300

    teste

--- infra/build.tf
+++ infra/build.tf
@@ -4,7 +4,7 @@ resource "null_resource" "build_dotnet_lambda" {
       dotnet restore /lambdaValidarUsuario/lambdaValidarUsuario.csproj
       dotnet publish /lambdaValidarUsuario/lambdaValidarUsuario.csproj -c Release -r linux-x64 --self-contained false -o /lambdaValidarUsuario/publish
     EOT
-    interpreter = ["/bin/sh", "-c"]
+    interpreter = ["PowerShell", "-Command"]
   }
 }
 
