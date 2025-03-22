f25a406959f41cec25cba6c84220dea051e5e85d
Gisele Cesar <gisele-cesar@hotmail.com>
2025-03-22 19:19:35 -0300

    Alteracao build.tf

--- infra/build.tf
+++ infra/build.tf
@@ -1,12 +1,9 @@
 resource "null_resource" "build_dotnet_lambda" {
   provisioner "local-exec" {
-    command     = <<EOT
-      dotnet restore ./lambdaValidarUsuario/lambdaValidarUsuario.csproj
-      dotnet publish ./lambdaValidarUsuario/lambdaValidarUsuario.csproj -c Release -r linux-x64 --self-contained false -o ./lambdaValidarUsuario/publish
+    command = <<EOT
+      dotnet restore lambdaValidarUsuario/lambdaValidarUsuario.csproj
+      dotnet publish lambdaValidarUsuario/lambdaValidarUsuario.csproj -c Release -r linux-x64 --self-contained false -o lambdaValidarUsuario/publish
     EOT
-    interpreter = ["PowerShell", "-Command"]
-  }
-  triggers = {
-    always_run = "${timestamp()}"
+    interpreter = ["/bin/sh", "-c"]
   }
 }
