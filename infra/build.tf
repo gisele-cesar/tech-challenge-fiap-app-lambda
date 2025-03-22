2bd919a725255beb7d7d4c86774ac9bf9b5ed19b
lmunarim <lmunarim@gmail.com>
2025-03-22 19:56:08 -0300

    teste

--- infra/build.tf
+++ infra/build.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "build_dotnet_lambda" {
   provisioner "local-exec" {
     command = <<EOT
-      dotnet restore lambdaValidarUsuario.csproj
+      dotnet restore /lambdaValidarUsuario/lambdaValidarUsuario.csproj
       dotnet publish lambdaValidarUsuario.csproj -c Release -r linux-x64 --self-contained false -o /publish
     EOT
     interpreter = ["/bin/sh", "-c"]
