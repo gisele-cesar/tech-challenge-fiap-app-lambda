1b859517a8faeaf1b3d30639eb78038a4010e7fe
lmunarim <lmunarim@gmail.com>
2025-03-22 17:37:45 -0300

    teste

--- infra/main.tf
+++ infra/main.tf
@@ -52,23 +52,23 @@ resource "aws_apigatewayv2_stage" "lambda" {
   auto_deploy = true
 }
 
-resource "aws_apigatewayv2_integration" "dotnet" {
-  api_id = aws_apigatewayv2_api.lambda.id
-  integration_uri    = aws_lambda_function.lambda.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
+# resource "aws_apigatewayv2_integration" "dotnet" {
+#   api_id = aws_apigatewayv2_api.lambda.id
+#   integration_uri    = aws_lambda_function.lambda.invoke_arn
+#   integration_type   = "AWS_PROXY"
+#   integration_method = "POST"
+# }
 
-resource "aws_apigatewayv2_route" "dotnet" {
-  api_id = aws_apigatewayv2_api.lambda.id
-  route_key = "POST /orders/{userId}/create"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
-}
+# resource "aws_apigatewayv2_route" "dotnet" {
+#   api_id = aws_apigatewayv2_api.lambda.id
+#   route_key = "POST /orders/{userId}/create"
+#   target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+# }
 
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
-}
+# resource "aws_lambda_permission" "api_gw" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.lambda.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+# }
