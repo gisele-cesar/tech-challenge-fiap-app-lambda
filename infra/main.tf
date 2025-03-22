5fd4f9696eb493cbddc4ff53f41331785e8cdfe6
lmunarim <lmunarim@gmail.com>
2025-03-22 17:28:31 -0300

    .

--- infra/main.tf
+++ infra/main.tf
@@ -54,7 +54,6 @@ resource "aws_apigatewayv2_stage" "lambda" {
 
 resource "aws_apigatewayv2_integration" "dotnet" {
   api_id = aws_apigatewayv2_api.lambda.id
-
   integration_uri    = aws_lambda_function.lambda.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
@@ -62,7 +61,6 @@ resource "aws_apigatewayv2_integration" "dotnet" {
 
 resource "aws_apigatewayv2_route" "dotnet" {
   api_id = aws_apigatewayv2_api.lambda.id
-
   route_key = "POST /orders/{userId}/create"
   target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
 }
@@ -72,6 +70,5 @@ resource "aws_lambda_permission" "api_gw" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
-
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
