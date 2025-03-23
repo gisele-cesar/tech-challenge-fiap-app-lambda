c9a780b1b45ae7a300641ba00c5a01733f8e3f21
lmunarim <lmunarim@gmail.com>
2025-03-23 16:53:36 -0300

    ...

--- infra/build.tf
+++ infra/build.tf
@@ -1,9 +1,9 @@
-resource "null_resource" "build_dotnet_lambda" {
-  provisioner "local-exec" {
-    command = <<EOT
-        dotnet publish -c Release -o ..\publish --runtime win-x64 --self-contained false
-    EOT
-    interpreter = ["/bin/sh", "-c"]
-  }
-}
+# resource "null_resource" "build_dotnet_lambda" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#         dotnet publish -c Release -o ..\publish --runtime win-x64 --self-contained false
+#     EOT
+#     interpreter = ["/bin/sh", "-c"]
+#   }
+# }
 
